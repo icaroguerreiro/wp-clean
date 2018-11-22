0a3f76387e705f399357eb5a2efcd93b8b1255f2
Ícaro Guerreiro <icaroguerreirocosta@gmail.com>
2018-11-22 15:24:24 -0300

    Small Fixes

--- header.php
+++ header.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-
-<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-
 <?php
 	// Phug <3
 	use JsPhpize\JsPhpizePhug;
@@ -17,18 +5,19 @@
 	Phug::addExtension(JsPhpizePhug::class);
 ?>
 
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<style><?php @include 'statics/css/critical.css'; ?></style>
+	<?php wp_head(); ?>
+	<?php	Phug::displayFile(__DIR__."/src/_head.pug"); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
 <div id="wrapper">
-
-	<header id="masthead" class="site-header" role="banner">
-
-		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
-
-		<?php if ( has_nav_menu( 'top' ) ) : ?>
-			<div class="navigation-top">
-				<div class="wrap">
-					<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
-				</div><!-- .wrap -->
-			</div><!-- .navigation-top -->
-		<?php endif; ?>
-
-	</header><!-- #masthead -->
+	<?php Phug::displayFile(__DIR__."/src/components/header/header.pug"); ?>
---
+++ src/core/js/helloworld.js
@@ -0,0 +1,2 @@
+const name = 'World'
+console.log(`%c-"Hello ${name}!" 👽`, "font-size:1.5em;")
